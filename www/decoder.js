1ebdce557de3eed3b36f1c172ca66ddcfb75a380
Jeff Cave <jefferey.cave@gmail.com>
2021-10-25 06:26:13 -0600

    Creating decoding from video stream

--- www/decoder.js
+++ www/decoder.js
@@ -2,77 +2,94 @@
 https://stackoverflow.com/questions/37996101/storing-binary-data-in-qr-codes#38323755
 */
 export {
-    Decode as default
+	Decode as default
 };
 
 import SplitHeader from "./lib/splitheader.js";
 import * as b45 from './lib/base45.js';
 
 const state = {
-    video: null
+	video: null
 };
 
 async function getMonitorSource(){
-    if(state.video) return state.video;
-    state.video = await navigator.mediaDevices.getDisplayMedia();
-    return state.video;
+	if(state.video) return state.video;
+	state.video = await navigator.mediaDevices.getDisplayMedia();
+	return state.video;
 }
 
 async function Decode(){
-    let stylesheet = document.createElement('link');
-    stylesheet.setAttribute('rel','stylesheet');
-    stylesheet.setAttribute('type','text/css');
-    stylesheet.setAttribute('href','decode.css');
-    document.head.append(stylesheet);
+	let stylesheet = document.createElement('link');
+	stylesheet.setAttribute('rel','stylesheet');
+	stylesheet.setAttribute('type','text/css');
+	stylesheet.setAttribute('href','decode.css');
+	//document.head.append(stylesheet);
 
-    const MAXSIZE = Math.floor(1555/b45.CompressionRatio)-SplitHeader.SIZE;
+	const MAXSIZE = Math.floor(1555/b45.CompressionRatio)-SplitHeader.SIZE;
 
-    const codeReader = new ZXing.BrowserDatamatrixCodeReader();
-    let stm = null;
-    let button = document.querySelector('button[name="decode"]');
-    let progress = document.querySelector('progress[name="decode"]');
+	const codeReader = new ZXing.BrowserDatamatrixCodeReader();
+	//const codeReader = new ZXing.BrowserQRCodeReader();
+	let button = document.querySelector('button[name="decode"]');
+	let progress = document.querySelector('progress[name="decode"]');
 
-    button.disabled = true;
+	button.disabled = true;
 
-    let imgs = await getMonitorSource();
-    let indexcard = null;
+	let camera = await getMonitorSource();
+	let indexcards = new Map();
 
-    let debugvid = document.querySelector('video[name="debug"]');
-    codeReader.decodeFromStream(imgs,debugvid,(result,err)=>{
-        if(err){
-            console.debug(err);
-        }
-        if(!result) return;
-        result = result.text;
-        result = b45.decode(result);
-        let header = new SplitHeader(result.buffer);
-        console.log(`${header.page} of ${header.pages}`);
-        let buf = new Uint8Array(result.buffer, header.SIZE);
+	let debugvid = document.querySelector('video[name="debug"]');
+	//debugvid.style.opacity = 0;
+	debugvid.style.display = 'block';
+	
+	codeReader.decodeFromStream(camera,debugvid,(result,err)=>{
+		if(err){
+			console.debug(err);
+		}
+		if(!result) return;
+		result = result.text;
+		result = b45.decode(result);
+		let header = new SplitHeader(result.buffer);
 
-        if(!stm){
-            let size = MAXSIZE * header.pages;
-            stm = new Uint8Array(size);
-            progress.setAttribute('max',header.pages);
-            indexcard = {
-                id: header.idString,
-                waiting: new Set([...Array(header.pages).keys()])
-            };
-        }
-        let offset = MAXSIZE * header.page;
-        stm.set(buf,offset);
-        indexcard.waiting.delete(header.page);
-        progress.value = indexcard.size;
+		let indexcard = indexcards.get(header.idString);
+		if(!indexcard){
+			let size = MAXSIZE * header.pages;
+			progress.setAttribute('max',header.pages);
+			indexcard = {
+				id: header.idString,
+				waiting: new Set([...Array(header.pages).keys()]),
+				stm: new Uint8Array(size)
+			};
+			indexcards.set(indexcard.id, indexcard);
+		}
+		
+		// if this item has already been processed, we can skip and wait for the next one
+		let pct = Math.floor(indexcard.waiting.size / header.pages * 1000)/10;
+		if(!indexcard.waiting.has(header.page)){
+			console.debug(`skip: ${header.page} : ${indexcard.waiting.size} of ${header.pages} (${pct}%)`);
+			return;
+		}
+		console.log(`${header.page} : ${indexcard.waiting.size} of ${header.pages} (${pct}%)`);
 
-        if(indexcard.waiting.size === 0){
-            this.stopContinuousDecode();
-            stm = new Blob([stm],{type:'application/epub+zip'});
-            saveAs(stm,`${id}.epub`);
-            button.disabled = false;
-            for(let stylesheet of document.querySelectorAll('link[href="decode.css"]')){
-                document.head.remove(stylesheet);
-            }
-        }
-    });
+		// we have a unique buffer, decode it
+		let buf = new Uint8Array(result.buffer, header.SIZE);
+		let offset = MAXSIZE * header.page;
+		// apply it to the larger stream
+		indexcard.stm.set(buf,offset);
+		indexcard.waiting.delete(header.page);
+		progress.value = indexcard.waiting.size;
 
-
+		if(indexcard.waiting.size === 0){
+			codeReader.stopContinuousDecode();
+			let stm = new Blob([indexcard.stm],{type:'application/epub+zip'});
+			saveAs(stm,`${indexcard.id}.epub`);
+			button.disabled = false;
+			for(let stylesheet of document.querySelectorAll('link[href="decode.css"]')){
+				document.head.remove(stylesheet);
+			}
+			for(let track of state.video.getTracks()){
+				track.stop();
+			}
+			debugvid.style.display = 'none';
+		}
+	});
 }
